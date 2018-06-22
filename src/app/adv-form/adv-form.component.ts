8e2aa9da4021b433cd1f9ae51c46cd187f9ce206
kishan.ks@hotmail.com <kishan91411>
2018-06-21 20:59:40 -0500

    final tag

--- src/app/adv-form/adv-form.component.ts
+++ src/app/adv-form/adv-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adv-form',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdvFormComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  // enthinicity: any[];
+  isSubmitted = false;
+
+  constructor(private router: Router) { 
+    // this.enthinicity = ["Asian", "American Indian", "African American"]
+    this.user = new User;
+  }
 
   ngOnInit() {
+  }
+
+  onFormSubmit({ value, valid }: { value: User, valid: boolean }) {
+    this.isSubmitted = true
+    this.user = value;
+    // console.log(this.user);
+    // console.log('valid: ' + valid);
+    if (valid) {
+      this.user = value;
+      console.log(this.user);
+      console.log('valid: ' + valid);
+      this.router.navigate(['/report'])
+      // this.dataService.saveUser(this.user)
+      //   .subscribe((data) => {
+      //     console.log(data)
+      //     console.log("success")
+      //     this.router.navigate(['/users'])
+      //   },
+      //     error => {
+      //       console.log("Error Occured")
+      //     });
+    }
   }
 
 }
--- src/app/basic-form/basic-form.component.ts
+++ src/app/basic-form/basic-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-basic-form',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicFormComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  isSubmitted = false;
+  gender = ["Select Gender", "Male", "Female", "Other"];
+  step: string;
+
+  constructor(private router: Router) {
+    this.user = new User;
+    this.user.gender = this.gender[0];
+    this.step = "1";
+  }
 
   ngOnInit() {
   }
 
+  onFormNext({ value, valid }: { value: User, valid: boolean }) {
+    this.isSubmitted = true
+    this.user = value;
+    this.step = "2";
+    
+  }
+
+  onFormSubmit({ value, valid }: { value: User, valid: boolean }) {
+    this.isSubmitted = true
+    this.user = value;
+    // console.log(this.user);
+    // console.log('valid: ' + valid);
+    if (valid) {
+      this.user = value;
+      console.log(this.user);
+      console.log('valid: ' + valid);
+      this.router.navigate(['/adv'])
+      // this.dataService.saveUser(this.user)
+      //   .subscribe((data) => {
+      //     console.log(data)
+      //     console.log("success")
+      //     this.router.navigate(['/users'])
+      //   },
+      //     error => {
+      //       console.log("Error Occured")
+      //     });
+    }
+  }
 }
